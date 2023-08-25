83fc2167177e25a03c2e342080d5e5443b073a60
ChihiroAida <142207215+ChihiroAida@users.noreply.github.com>
2023-08-26 00:37:24 +0900

    1-3

--- JavaStudy/1-3/Task1_3.java
+++ JavaStudy/1-3/Task1_3.java
@@ -7,46 +7,46 @@ public class Task1_3 {
 		
 		 // ① 5 と 10 を加算した値を代入する、変数 plus を作成しなさい。
 		
-			int plus = 5 + 10;
+		int plus = 5 + 10;
 
-        // ② 20 から 7 を減算した値を代入する、変数 minus を作成しなさい。
-			
-			int minus = 20 - 7;
-			
-
-        // ③下記の処理について、何をしているのかコメントを記入してください。
-        //10 と 2 を乗算した値を代入する、変数 multiply を作成している。
-			
-			int multiply = 10 * 2;
-        
-
-        // ④ 20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
-        
-        	int remi = 20;
+    // ② 20 から 7 を減算した値を代入する、変数 minus を作成しなさい。
 		
-        	remi %= 6; 
+		int minus = 20 - 7;
+		
 
-        // ⑤下記の処理について、何をしているのかコメントを記入してください
-        // Stringクラスの town という名前の変数を宣言、値「中央」で初期化
-        // Stringクラスの line という名前の変数を宣言、値「線」で初期化
-        	
-        	String town = "中央";
-        	String line = "線";
+    // ③下記の処理について、何をしているのかコメントを記入してください。
+    //10 と 2 を乗算した値を代入する、変数 multiply を作成している。
+		
+		int multiply = 10 * 2;
+    
 
-        // ⑥ ⑤で作成した変数を連結させた値を代入する変数 train を作成しなさい。
-        	
-        	String train = town + line;
-        
+    // ④ 20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
+    
+    	int remi = 20;
+	
+    	remi %= 6; 
 
-        // ⑦ 変数plus, minus, multiply, remi, train をそれぞれ出力しなさい。
+    // ⑤下記の処理について、何をしているのかコメントを記入してください
+    // Stringクラスの town という名前の変数を宣言、値「中央」で初期化
+    // Stringクラスの line という名前の変数を宣言、値「線」で初期化
+    	
+    	String town = "中央";
+    	String line = "線";
 
-        	System.out.println(plus);
-        	System.out.println(minus);
-        	System.out.println(multiply);
-        	System.out.println(remi);
-        	System.out.println(train);
+    // ⑥ ⑤で作成した変数を連結させた値を代入する変数 train を作成しなさい。
+    	
+    	String train = town + line;
+    
 
-        	
-	}		
+    // ⑦ 変数plus, minus, multiply, remi, train をそれぞれ出力しなさい。
+
+    	System.out.println(plus);
+    	System.out.println(minus);
+    	System.out.println(multiply);
+    	System.out.println(remi);
+    	System.out.println(train);
+
+
+	}
 
 }
