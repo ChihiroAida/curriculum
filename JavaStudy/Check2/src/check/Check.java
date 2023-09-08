cf0dace72602c6aac21fc9c3abdc87a038adf3aa
ChihiroAida <142207215+ChihiroAida@users.noreply.github.com>
2023-09-08 12:42:20 +0900

    Check2

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,31 @@
+package check;
+
+import constants.Constants;
+
+public class Check {
+	
+	private String firstName = "会田";
+	private String lastName= "千紘";
+
+	
+	private void printName() {
+		
+		System.out.println("printNameメソッド → " + firstName+lastName);
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		Check check = new Check();
+		check.printName();
+		
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+		
+		RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+		robotpet.introduce();
+
+	}
+
+}
