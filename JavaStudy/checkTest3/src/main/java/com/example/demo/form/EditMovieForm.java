a62f98f9bb50fe197acbe9398389e21f9c50bb23
ChihiroAida <142207215+ChihiroAida@users.noreply.github.com>
2024-01-02 00:18:14 +0900

    Create EditMovieForm.java

---
+++ JavaStudy/checkTest3/src/main/java/com/example/demo/form/EditMovieForm.java
@@ -0,0 +1,31 @@
+package com.example.demo.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class EditMovieForm {
+
+	
+    @NotNull(message="IDがNullになっています")
+    private Integer id;
+	
+    @NotBlank(message="タイトルを入力してください。")
+    private String title;
+    
+    @NotNull(message = "評価を入力してください")
+	private Integer evaluation;
+	
+    @NotBlank(message="国を入力してください。")
+    private String place;
+    
+    @NotBlank(message="鑑賞方法を入力してください。")
+    private String way;
+    
+	@Size(max = 50, message = "50文字以内で入力してください")
+	private String memo;
+	
+}
